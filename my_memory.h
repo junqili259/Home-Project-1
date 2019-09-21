79e312264e7264e9d76ebfed488f0ef5f8516945
junqi <junqili259@gmail.com>
2019-09-21 04:07:55 -0400

    Header file

---
+++ my_memory.h
@@ -0,0 +1,27 @@
+/*
+Jun Qi Li
+CSCI 335
+Home Project 1
+my_memory.h
+*/
+#ifndef MY_MEMORY_H
+#define MY_MEMORY_H
+
+template<typename T>
+class my_unique_ptr{
+    public:
+    private:
+
+}
+
+template<typename Y>
+class my_shared_ptr{
+    private:
+    public:
+}
+
+
+
+#include "my.memory.cpp"
+#endif
+
