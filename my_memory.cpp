c1631a87dc86343a938e20e8b67797907c026c5c
junqi <junqili259@gmail.com>
2019-09-21 04:42:57 -0400

    Implementation File

---
+++ my_memory.cpp
@@ -0,0 +1,23 @@
+/*
+Jun Qi Li
+CSCI 335
+Home Project 1
+my_memory_cpp
+*/
+#include "my_memory.h"
+
+//Default Constructor
+//Initialize pointer to null
+template<typename T>
+my_unique_ptr<T>::my_unique_ptr(): unique_ptr(nullptr){}
+
+
+//template<typename T>
+//my_unique_ptr<T>::my_unique_ptr(T* )
+
+
+////////////////////////////////////////////////////////////////////////////////
+template<typename Y>
+my_shared_ptr<Y>::my_shared_ptr(): shared_ptr(nullptr),counter_ptr(nullptr){}
+
+
