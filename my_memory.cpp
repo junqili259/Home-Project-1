7d49d5be665ad9d362fd790df8f3bfa9c7985671
junqili <39481617+junqili259@users.noreply.github.com>
2019-09-28 03:34:05 -0400

    Add files via upload

--- my_memory.cpp
+++ my_memory.cpp
@@ -12,7 +12,7 @@ template<class T>
 my_unique_ptr<T>::my_unique_ptr(): unique_ptr(nullptr){}
 
 
-//Constructor for class my_unique_ptr
+//Parameterized Constructor for class my_unique_ptr
 template<class T>
 my_unique_ptr<T>::my_unique_ptr(T* a_ptr): unique_ptr(a_ptr){}
 
@@ -26,29 +26,33 @@ my_unique_ptr<T>::my_unique_ptr(my_unique_ptr<T>&& other_obj):unique_ptr(other_o
 
 
 //Move assignment operator for class my_unique_ptr
-
 template<class T>
 my_unique_ptr<T>& my_unique_ptr<T>::operator=(my_unique_ptr<T>&& other_obj)
 {
-    my_unique_ptr<T> temp_ptr;
-    temp_ptr.unique_ptr = unique_ptr;
-    unique_ptr = other_obj.unique_ptr;
-    other_obj.unique_ptr = nullptr;
-    delete temp_ptr.unique_ptr;
+   if(this == &other_obj){
+       return *this;
+   }
+   else
+   {
+       unique_ptr = other_obj.unique_ptr;
+       other_obj.unique_ptr = nullptr;
+       return *this;
+   }
 }
 
 
-
-
+//Test Function
+//test for null pointer
 template<class T>
 bool my_unique_ptr<T>::isNullptr(){
     if (unique_ptr == nullptr)
     {
         return true;
     }
+    else return false;
 }
 
-
+//Deference operator overloaded
 template<class T>
 T& my_unique_ptr<T>::operator*()
 {
@@ -56,7 +60,7 @@ T& my_unique_ptr<T>::operator*()
 }
 
 
-
+//Arrow operator overloaded
 template<class T>
 T* my_unique_ptr<T>::operator->()
 {
@@ -72,9 +76,116 @@ my_unique_ptr<T>::~my_unique_ptr(){
 }
 
 
-////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Default constructor for class my_shared_ptr
-//template<typename Y>
-//my_shared_ptr<Y>::my_shared_ptr(): shared_ptr(nullptr),counter_ptr(nullptr){}
+template<class Y>
+my_shared_ptr<Y>::my_shared_ptr(): shared_ptr(nullptr), counter_ptr(nullptr){}
+
+
+//parameterized constructor
+template<class Y>
+my_shared_ptr<Y>::my_shared_ptr(Y* ptr): shared_ptr(ptr), counter_ptr(new int{1}){}
+
+
+//copy constructor
+template<class Y>
+my_shared_ptr<Y>::my_shared_ptr(const my_shared_ptr<Y>& other_obj)
+{
+  shared_ptr = other_obj.shared_ptr;
+  counter_ptr = other_obj.counter_ptr;
+  *counter_ptr+=1;
+}
+
+
+//copy assignment
+template<class Y>
+my_shared_ptr<Y> & my_shared_ptr<Y>::operator=(const my_shared_ptr<Y>& other_obj)
+{
+  if (this == &other_obj)
+  {
+    return *this;
+  }
+  else
+    delete shared_ptr;
+    delete counter_ptr;
+
+    shared_ptr = other_obj.shared_ptr;
+    counter_ptr = other_obj.counter_ptr;
+    *counter_ptr+=1;
+    return *this;
+}
+
+
+
+//Move constructor
+template<class Y>
+my_shared_ptr<Y>::my_shared_ptr(my_shared_ptr<Y>&& other_obj): shared_ptr(other_obj.shared_ptr), counter_ptr(other_obj.counter_ptr)
+{
+  other_obj.shared_ptr = nullptr;
+  other_obj.counter_ptr = nullptr;
+}
+
+
+
+//Move Assignment
+template<class Y>
+my_shared_ptr<Y> & my_shared_ptr<Y>::operator=(my_shared_ptr<Y>&& other_obj)
+{
+  if (shared_ptr != nullptr)
+  {
+    *counter_ptr-=1;
+    if (*counter_ptr == 0)
+    {
+      //delete counter_ptr;
+      //delete shared_ptr;
+      this->~my_shared_ptr();
+    }
+  }
+  
+  shared_ptr = other_obj.shared_ptr;
+  counter_ptr = other_obj.counter_ptr;
+  return *this;
+
+}
+
+
+//Test function
+//returns number of pointers pointing to the same object
+template<class Y>
+int my_shared_ptr<Y>::getCount()
+{
+  return *counter_ptr;
+}
+
+//Overloaded deference operator
+
+template<class Y>
+Y& my_shared_ptr<Y>::operator*()
+{
+	return *shared_ptr;
+}
+
+
+template<class Y>
+Y* my_shared_ptr<Y>::operator->()
+{
+	return shared_ptr;
+}
+
+
+
+
+//Destructor for my_shared_ptr class
+template<class Y>
+my_shared_ptr<Y>::~my_shared_ptr(){
+  
+    //deletes dynamic memory when no pointers are left pointing to the object
+    if (*counter_ptr == 0)
+    {
+      delete counter_ptr;
+      delete shared_ptr;
+    }
+  
+}
 
 
--- my_memory.h
+++ my_memory.h
@@ -13,29 +13,43 @@ class my_unique_ptr{
         my_unique_ptr();//default constructor
         my_unique_ptr(T* a_ptr);//constructor
         my_unique_ptr(const my_unique_ptr<T>&) = delete;//copy constructor
-        my_unique_ptr & operator=(const my_unique_ptr<T>& rhs) = delete;//copy assignment operator
+        my_unique_ptr & operator=(const my_unique_ptr<T>&) = delete;//copy assignment operator
         my_unique_ptr(my_unique_ptr<T>&& other_obj); //move constructor
         my_unique_ptr& operator=(my_unique_ptr<T>&& other_obj); //move assignment operator
         T& operator*(); //deference operator
         T* operator->();//Operator -> overloaded
-        bool isNullptr();
         ~my_unique_ptr();//destructor
+
+
+        //Test functions
+        bool isNullptr();//test for null pointer
     private:
         T* unique_ptr;
 
 };
 
-/*
-template<typename Y>
+
+template<class Y>
 class my_shared_ptr{
     public:
         my_shared_ptr();//default constructor
+        my_shared_ptr(Y* ptr);//constructor
+        my_shared_ptr(const my_shared_ptr<Y>& other_obj);//copy constructor
+        my_shared_ptr & operator=(const my_shared_ptr<Y>& other_obj);//copy assignment
+        my_shared_ptr(my_shared_ptr<Y>&& other_obj);// move constructor
+        my_shared_ptr & operator=(my_shared_ptr<Y>&& other_obj);
+        Y& operator*();//deference operator
+        Y* operator->();//Operator -> overloaded
         ~my_shared_ptr();//destructor
+
+        //Test functions
+        int getCount();
+
     private:
         Y* shared_ptr;
         int* counter_ptr;
 };
-*/
+
 
 
 
